7d9a705ccbaf0bccec12edc34298a0d9ccc9a661
Esther-Naserian <yemanehaxumawit@gmail.com>
2024-06-23 22:01:31 +0300

    made changes

--- src/Courses/index.js
+++ src/Courses/index.js
@@ -7,44 +7,38 @@ import science from "../images/science two.jpeg";
 import kiswahili from "../images/kiswahili.gif";
 import rightArrow from "../images/right-arrow-backup-2-svgrepo-com.svg";
 import leftArrow from "../images/left-arrow-backup-2-svgrepo-com (1).svg";
-import React, {useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 
 const Courses = () => {
 
-    const coursesList = useRef(null)
-
-    const scrollLeft  = ()=> {
-        if (coursesList.current){
-            coursesList.current.scrollBy({
-                left:-40,
-                behavior:'smooth',
-            });
-            console.log(scrollLeft);
-        }
+    const coursesListRef = useRef(null);
+    const leftArrowRef = useRef(null);
+    const rightArrowRef = useRef(null);
+    
+   useEffect(()=>{
+    const scrollLeft = (event) => {
+    coursesListRef.current.scrollLeft -=1000
     };
-    const scrollRight  = ()=> {
-        if (coursesList.current){
-            coursesList.current.scrollBy({
-                left:40,
-                behavior:'smooth',
-            });
-            console.log(scrollRight);
-        }
-    };
-
+    const scrollRight = (event) => {
+        coursesListRef.current.scrollRight +=1000
+        };
+    const leftButton = leftArrowRef.current;
+    const rightButton = rightArrowRef.current;
+    leftButton.addEventListener("click",scrollLeft);
+    rightButton.addEventListener("click",scrollRight);
+   },[]);
 
     return (
         
         <div className="courses-container">
             <div className='courses'>Popular Courses</div>
             <div className='details'>
-            <div className='leftArrow' onClick={scrollLeft}>
+            <div className='leftArrow' ref={leftArrowRef}>
         <img src= {leftArrow} alt ="leftArrow" className="arrows"/>
     </div>
-           <div className="courses-list-wrapper">
-                <div className='list'>
-                <div className="courses-list" ref={coursesList}>
+           <div className="courses-list-wrapper" ref={coursesListRef}>
+                <div className="courses-list">
                     <li id="english">
                         <img src={english} alt="english" className="image" />
                         <h2 className="englishh">English</h2>
@@ -70,9 +64,8 @@ const Courses = () => {
                         <h2 className="cree">C.R.E</h2>
                     </li>
                 </div>
-                </div>
             </div>
-            <div className='rightArrow' onClick={scrollRight}>
+            <div className='rightArrow' ref={rightArrowRef}>
         <img src= {rightArrow} alt ="rightArrow" className="arrows"/>
     </div>
     </div>
